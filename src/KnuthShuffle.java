d52280c4321d3b30433bccc65ceca5f9de211aea
Mandeep Condle <mandeepcondle@gmail.com>
2014-07-04 23:50:08 -0400

    Knuth shuffle

---
+++ src/KnuthShuffle.java
@@ -0,0 +1,17 @@
+/**
+ * Method to shuffle a deck of cards based on Donald Knuth's algorithm
+ * to swap the ith card with a random card between 0 and is
+ */
+
+public class KnuthShuffle {
+
+    public static void main(String[] args) {
+
+        Card[] cards = new Card[52];
+
+
+
+
+    }
+
+}
